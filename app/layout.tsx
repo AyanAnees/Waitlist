fd939a25df538e35697bbd5f3750a09955eec8d1
Yash Patki <yashom.patki@gmail.com>
2024-09-01 21:35:15 -0700

    Removed prev icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,27 +2,24 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Poppins } from "next/font/google";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 const poppins = Poppins({ subsets: ["latin"], weight: ["500"] });
 
 export const metadata: Metadata = {
-  title: "contribu | waitlist",
-  description: "waitlist for contribu, a platform for finding and collaborating on projects.",
+    title: "contribu | waitlist",
+    description:
+        "waitlist for contribu, a platform for finding and collaborating on projects.",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-      <Head>
-          <link rel="icon" href="/favicon.ico" />
-          <html lang="en">
-              <body className={`${poppins.className}`}>{children}</body>
-          </html>
-      </Head>
-  );
+    return (
+        <html lang="en">
+            <body className={`${poppins.className}`}>{children}</body>
+        </html>
+    );
 }
